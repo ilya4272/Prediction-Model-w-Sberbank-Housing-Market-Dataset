f1cf0210aeae74b21ffd061ef8bcf6cb844c6fc6
ilya-naga <luoteeseen8@gmail.com>
2024-08-05 23:16:12 +0200

    Create config file

---
+++ Code/config.py
@@ -0,0 +1,2 @@
+path_1 = "C:\\Users\\ilyas\\Documents\\Data_Science\\Projects\\Kaggle\\Prediction\\Project_1\\Raw_Data\\Model_1_Prediction_Model_sberbank-russian-housing-market\\"
+comp = "sberbank-russian-housing-market"
