c02a41cbd543ec4fb9ba5251bc65b3ef97a09810
ilya-naga <luoteeseen8@gmail.com>
2024-08-06 20:17:00 +0200

    Start EDA

--- Code/config.py
+++ Code/config.py
@@ -1,2 +1,4 @@
 path_1 = "C:\\Users\\ilyas\\Documents\\Data_Science\\Projects\\Kaggle\\Prediction\\Project_1\\Raw_Data\\Model_1_Prediction_Model_sberbank-russian-housing-market\\"
+path_2 = "C:\\Users\\ilyas\\Documents\\Data_Science\\Projects\\Kaggle\\Prediction\\Project_1\\Raw_Data\\Model_1_Prediction_Model_sberbank-russian-housing-market\\extracted"
+path_3 = "C:\\Users\\ilyas\\Documents\\Data_Science\\Projects\\Kaggle\\Prediction\\Project_1\\Raw_Data\\"
 comp = "sberbank-russian-housing-market"
